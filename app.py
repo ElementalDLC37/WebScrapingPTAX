94d48edda9c5a3aa0fda5d50f80fe400dce214a1
Rui Gomes <lucasantony815@gmail.com>
2023-08-18 22:48:23 -0300

    set

--- app.py
+++ app.py
@@ -1,12 +1,53 @@
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
 import bcb
-import json
+import pandas as pd
+from pandas.tseries.offsets import BDay
+import numpy
+import investpy
+import datetime
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/")
-def hello_world():
-    ptax = bcb.PTAX().get_endpoint('CotacaoMoedaDia')
-    df = ptax.query().parameters(moeda='USD', dataCotacao='03/08/2022').collect()
-    cotacao_venda_list = df['cotacaoVenda'].values.tolist()
-    return { "ptax": cotacao_venda_list }
+@app.route("/ptax")
+def get_PTAX():
+    endpoint = bcb.PTAX().get_endpoint('CotacaoMoedaDia')
+
+    if request.args.get('date'):
+        day_request = request.args.get('date')
+        date = day_request
+    else:
+        last_bussines_day = (pd.Timestamp.today() - BDay(1)).strftime("%m/%d/%Y")
+        date = last_bussines_day
+
+
+    ptax = endpoint.query().parameters(moeda='USD', dataCotacao=date).collect()
+    
+    if len(ptax.keys()) != 0:
+        print(True)
+    else:
+        return { "code": 404 }
+
+    ptax = endpoint.query().parameters(moeda='USD', dataCotacao=date).collect()
+
+    list = ptax['cotacaoVenda'].values.tolist()
+
+    return {
+        "code": 200,
+        "date": date,
+        "interm": {
+            "10:00": str(list[0] * 1000), 
+            "11:00": str(list[1] * 1000), 
+            "12:00": str(list[2] * 1000), 
+            "13:00": str(list[3] * 1000)  
+        }, 
+        "mean": numpy.mean(list) * 1000,
+        "std": numpy.std(list) * 1000
+    }
+
+@app.route("/economic-calendar")
+def get_economic_calendar():
+    data = investpy.news.economic_calendar(from_date="11/08/2023", to_date="12/08/2023", countries=["United States", "Brazil"])
+    
+    return data.to_json()
